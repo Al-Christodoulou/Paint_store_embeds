bab4007ef1413a16ba849bd8209d3bcc87c63e18
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2024-09-25 14:53:52 +0300

    Update paint_products.js, products_page.js, script.js, and 1 more file

---
+++ products_page.js
@@ -0,0 +1,40 @@
+
+const createProductsPage = (page_title) => {
+	content = `
+		<h1 class="system-ui-font blue-blurry-shadow" style="color: white; text-align: center;">${page_title}</h1>
+		<div style="text-align: center;">
+			<input type="text" class="ridge-border system-ui-font" style="font-size: small; padding: 7px;" id="searchInput" placeholder="Τι ψάχνετε; 🔎">
+			<div style="min-height: 50px;"></div>
+		</div>
+		`;
+	content += createPaginationDiv(false);
+	content += `
+		<div id="products" class="horz-flex-container">
+			<div id="product-filters" class="product-filters"></div>
+			<div id="product-listing" style="max-width: 1200px;"></div>
+		</div>
+	`;
+	content += createPaginationDiv(true);
+	content += `<div style="height: 30px;"></div>`;
+
+	return content;
+}
+
+const createPaginationDiv = (paddingAtBegin) => {
+	const padding = `<div style="height: 10px;"></div>`;
+	const divContent = `
+		<button class="prevPage page-buttons system-ui-font">Προηγούμενο</button>
+		<span class="system-ui-font blue-blurry-shadow pageNumber" style="color: white;">1</span>
+		<button class="nextPage page-buttons system-ui-font">Επόμενο</button>
+	`;
+
+	paginationDiv = `<div class="pagination">`;
+	paddingAtBegin ?
+		paginationDiv += padding + divContent :
+		paginationDiv += divContent + padding;
+	paginationDiv += `</div>`
+	return paginationDiv;
+}
+
+// Now create the page
+document.body.insertAdjacentHTML('beforeend', createProductsPage(document.currentScript.getAttribute('title')));
---
+++ script.js
@@ -0,0 +1,123 @@
+
+let currentPage = 1;
+const itemsPerPage = 10;
+let selectedFilters = [];
+let filteredProducts = [...Products];
+
+const renderFilters = () => {
+	const product_filters = document.getElementById("product-filters")
+	let categories = Products.map(product => product.category);
+	categories = new Set(categories);
+
+	// Create the product filters
+	let i = 0;
+	categories.forEach(category => {
+		// Don't make a category from an empty string
+		if (category.length === 0)
+			return;
+
+		const buttonsHTML = `
+			<div class="ridge-border-squared" style="padding: 2px; margin-bottom: 10px;">
+				<input type="checkbox" id="filter${i}" name="filter${i}" value="${category}">
+				<label class="system-ui-font" style="color: white;" for="filter${i}">${category}</label><br>
+			</div>
+		`
+		product_filters.innerHTML += buttonsHTML
+		i++
+	});
+}
+
+const renderProducts = () => {
+	const start = (currentPage - 1) * itemsPerPage;
+	const end = start + itemsPerPage;
+	const visibleProducts = filteredProducts.slice(start, end);
+
+	const productList = document.getElementById('product-listing');
+	productList.innerHTML = '';
+
+	visibleProducts.forEach(product => {
+		// Create the HTML structure using template literals
+		const productsHTML = `
+		<div class="horz-flex-container ridge-border product-padding" style="position: sticky;">
+			<div class="vert-flex-container">
+				<img class="product-image product-border" src="${product.image}"/>
+				<h2 class="system-ui-font h2-centered blue-blurry-shadow">${product.name}</h2>
+			</div>
+			<div>
+				<p class="small-leftright-padding system-ui-font blue-blurry-shadow">${product.description}</p>
+				<p class="small-leftright-padding system-ui-font blue-blurry-shadow">Ποσότητες: ${product.amount}</p>
+			</div>
+		</div>
+		`;
+
+		// Append the generated HTML to the 'products' div
+		productList.innerHTML += productsHTML;
+	});
+
+	let pageNumbers = document.getElementsByClassName('pageNumber');
+	for (let i = 0; i < pageNumbers.length; i++) {
+		pageNumbers[i].textContent = `${currentPage} / ${getTotalPages()}`;
+	}
+};
+
+const filterProducts = () => {
+	const searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+	filteredProducts = Products.filter(product => {
+		const matchesCategory = selectedFilters.length === 0 || selectedFilters.includes(product.category);
+		const matchesSearch = product.name.toLowerCase().includes(searchInput);
+		return matchesCategory && matchesSearch;
+	});
+
+	currentPage = 1;  // Reset to first page after filter
+	renderProducts();
+};
+
+const getTotalPages = () => {
+	return Math.ceil(filteredProducts.length / itemsPerPage);
+}
+
+document.getElementById('searchInput').addEventListener('input', filterProducts);
+
+const nextPageBtns = document.getElementsByClassName('nextPage');
+for (let i = 0; i < nextPageBtns.length; i++) {
+	nextPageBtns[i].addEventListener('click', () => {
+		const totalPages = getTotalPages();
+		if (currentPage < totalPages) {
+			currentPage++;
+			renderProducts();
+			document.getElementById('searchInput').scrollIntoView();
+		}
+	});
+}
+
+const prevPageBtns = document.getElementsByClassName('prevPage');
+for (let i = 0; i < prevPageBtns.length; i++) {
+	prevPageBtns[i].addEventListener('click', () => {
+		if (currentPage > 1) {
+			currentPage--;
+			renderProducts();
+			document.getElementById('searchInput').scrollIntoView();
+		}
+	});
+}
+
+// Initial render
+renderFilters();
+renderProducts();
+
+// Add the functionality to the product filters after the initial render
+document.querySelectorAll('#product-filters input[type="checkbox"]').forEach(button => {
+	button.addEventListener('change', () => {
+		const category = button.getAttribute('value');
+
+		if (selectedFilters.includes(category) ?
+			selectedFilters = selectedFilters.filter(s => s !== category) :
+			selectedFilters.push(category)
+		);
+		// this only changes the ticked/unticked box
+		button.click();
+
+		filterProducts();
+	});
+});
