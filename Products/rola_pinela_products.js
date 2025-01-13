e1fd2086c940d9115662f8ef1d0fcfdbf603b4f2
AlexChr2 <99661503+AlexChr2@users.noreply.github.com>
2025-01-14 01:05:07 +0200

    adding a new product category

---
+++ Products/rola_pinela_products.js
@@ -0,0 +1,60 @@
+const Products = [
+	/* Ρολά */
+	{
+		name: "Ρολό Morris Classic",
+		description: "Δερμάτινο ρολό Morris Classic 3 αστέρων για όλες τις χρήσεις. Ειδικό για όλα τα υδροδιαλυτά χρώματα, λαδομπογιές, βερνίκια, κόλλες κ.λ.π.",
+		category: "Ρολό",
+		amount: "",
+		image: "https://s6.imgcdn.dev/pKEWd.webp",
+	},
+	{
+		name: "Ρολό βαφής Rollex Promax",
+		description: "Ρολό ιδανικό για όλα τα χρώματα νερού και ασβέστη.",
+		category: "Ρολό",
+		amount: "",
+		image: "https://s6.imgcdn.dev/pfqun.webp",
+	},
+	{
+		name: "Rollex Rodan Ρολό",
+		description: "",
+		category: "Ρολό",
+		amount: "",
+		image: "https://s6.imgcdn.dev/pfFag.webp",
+	},
+	{
+		name: "Rollex Texture Economy Σπατουλαριστό Ρολό",
+		description: "Rollex texture economy σπατουλαριστό. Ρολό βαφής κατάλληλο για χρώματα νερού και λαδιού για σπατουλαριστές επιφάνειες λείες ή ελαφρώς άγριες. Διάμετρος σωλήνα Ø50, ύψος πέλους 1cm και μήκος πέλους 18cm. Εύκολο στη χρήση και στο καθάρισμα με εκπληκτική αντοχή. Κατάλληλο για επαγγελματίες.",
+		category: "Ρολό",
+		amount: "",
+		image: "https://s6.imgcdn.dev/pfHWv.webp",
+	},
+	{
+		name: "Ρολό Mohair Benman",
+		description: "Ρολό Mohair κατασκευασμένο από 100% φυσικό μαλλί. Προσφέρει άριστο φινίρισμα και είναι κατάλληλο για ριπολίνες, βερνίκια/λάκες και ντουκοχρώματα.",
+		category: "Ρολό",
+		amount: "",
+		image: "https://s6.imgcdn.dev/pfghN.webp",
+	},
+	/* Πινέλα */
+	{
+		name: "Πινέλο Α114 Benman",
+		description: "Οικονομικό πινέλο, ημίδιπλο και με συνδυασμό φυσικής και συνθετικής τρίχας. Πλαστική λαβή που δεν επηρεάζεται από διαλύτες. Εποξική κόλλα στο στροφείο. Κατάλληλο για υδατοδιαλυτά χρώματα (πλαστικά, ακρυλικά, ριπολίνες και βερνίκια νερού)",
+		category: "Πινέλα",
+		amount: "",
+		image: "https://s6.imgcdn.dev/pfEKq.webp",
+	},
+	{
+		name: "Morris Πινέλο Βαψίματος A15",
+		description: "Το πινέλο βαψίματος Morris A15 με πλάτος 12mm είναι ένα αξιόπιστο εργαλείο για τις βαφές σου. Προσφέρει ακρίβεια και ομοιόμορφη κάλυψη σε διάφορες επιφάνειες. Είναι κατάλληλο για επαγγελματίες και ερασιτέχνες, εξασφαλίζοντας ποιοτικό αποτέλεσμα στις εργασίες σου.",
+		category: "Πινέλα",
+		amount: "",
+		image: "https://s6.imgcdn.dev/pfJ8u.webp",
+	},
+	{
+		name: "Morris Μπατανόβουρτσα Βαψίματος",
+		description: "",
+		category: "Βούρτσα",
+		amount: "",
+		image: "https://s6.imgcdn.dev/ptDra.webp",
+	},
+]
