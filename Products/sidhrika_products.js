58527ba37593004159fce080282640872a68f74e
AlexChr2 <99661503+AlexChr2@users.noreply.github.com>
2025-01-26 22:12:00 +0200

    adding a new ironware category

---
+++ Products/sidhrika_products.js
@@ -0,0 +1,9 @@
+const Products = [
+	{
+		name: "Γωνίες Μαραγκών",
+		description: "Διαστάσεις 20x20, 30x30, 40x40, 50x50, 60x60, 80x80, 100x100.",
+		category: "Γωνίες",
+		amount: "",
+		image: "https://s6.imgcdn.dev/YDWejT.webp",
+	},
+]
